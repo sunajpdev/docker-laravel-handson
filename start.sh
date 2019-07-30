1b0ffc14c3c62787778697c2761bad3e4001f4a3
sunajpdev <sunajp.dev@gmail.com>
2019-07-31 08:46:52 +0900

    fix error app key

--- start.sh
+++ start.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+
+# copy docker env 
+cp env.example .env
+
+# start docker
+docker-compose up -d
+
+# install laravel vender
 docker-compose exec app composer install
+
+# copy laravel env
 docker-compose exec app cp .env.example .env
+
+# set app key
 docker-compose exec app php artisan key:generate
+
+
+
